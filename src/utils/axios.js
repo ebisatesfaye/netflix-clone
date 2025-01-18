9b07b2499a5da9d75a34bbe63e77b869751572e0
ebisa <ebisatesfaye30@gmail.com>
2025-01-18 11:22:58 +0300

    api url adjustment

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,3 +3,8 @@ const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
 });
 export default instance;
+
+
+// full link to api videos = https://api.themoviedb.org/3/discover/movie?api_key=307cc7c5fd582f7bdbf7b3ac93ad1de5&with_genres=28
+
+// https://api.themoviedb.org/3/discover/tv?api_key=307cc7c5fd582f7bdbf7b3ac93ad1de5 &with_networks=213
